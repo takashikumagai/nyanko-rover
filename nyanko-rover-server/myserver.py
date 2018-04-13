9539426eaa601117f5a07c94c6898c1144283b3f
nyankosoft <sgt.nyanko@gmail.com>
2018-04-13 18:22:43 +0900

    Cleaned up the source code.

--- nyanko-rover-server/myserver.py
+++ nyanko-rover-server/myserver.py
@@ -5,8 +5,6 @@ import os
 import logging
 import inspect
 
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
-
 import motor_control
 
 
@@ -32,26 +30,6 @@ class NyankoRoverHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     rootdir = guess_script_file_directory()
     try:
       logging.debug('self.path {}'.format(self.path))
-      # if self.path == '/' or self.path.endswith('.html'):
-      #   mypath = 'index.html' if self.path == '/' else self.path
-      #   with open(os.path.join(rootdir,mypath)) as f:
-      #     s = f.read()
-      #
-      #     send_response_and_header('text/html')
-      #
-      #     #send file content to client
-      #     logging.debug('file content: {}'.format(s))
-      #     self.wfile.write(s.encode('utf-8'))
-      #     #self.wfile.write(b"<html><body><h1>nyanko server</h1></body></html>")
-      #     return
-      #
-      # elif self.path.endswith('.js'):
-      #   with open(os.path.join(rootdir,mypath)) as f:
-      #     s = f.read()
-      #     send_response_and_header('application/javascript')
-      #     logging.debug('js content: {}'.format(s))
-      #     self.wfile.write(s.encode('utf-8'))
-      #     return
 
       if self.path.startswith('/forward'):
         print('forward action requested')
@@ -69,19 +47,6 @@ class NyankoRoverHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     except IOError:
       motor_control.stop_motor()
       self.send_error(404, 'file not found')
-
-class SimpleEcho(WebSocket):
-
-    def handleMessage(self):
-        print("Message received: {}".format(self.data))
-        # echo message back to client
-        self.sendMessage(self.data)
-
-    def handleConnected(self):
-        print(self.address, 'connected')
-
-    def handleClose(self):
-        print(self.address, 'closed')
 
 def start():
 
