dc43d638b9eed8e0b7773a0ddb5b11e96be7e71b
nyankosoft <sgt.nyanko@gmail.com>
2018-04-27 12:41:52 +0900

    Added rover control functions (JavaScript).

---
+++ nyanko-rover-server/nr_rover_control.js
@@ -0,0 +1,65 @@
+// Dependencies:
+//   nr_websocket.js
+
+function moveForward() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET","forward");
+  xmlhttp.send();
+}
+
+function moveBackward() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET","backward?duration=3");
+  xmlhttp.send();
+}
+
+function steerLeft() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET","steer?dir=left");
+  xmlhttp.send();
+}
+
+function steerRight() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET","steer?dir=right");
+  xmlhttp.send();
+}
+
+function stop() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET","stop");
+  xmlhttp.send();
+  //myWebSocket.send("sending message via WebSocket");
+}
+
+function takePhoto() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET","take_photo");
+  xmlhttp.send();
+  //myWebSocket.send("sending message via WebSocket");
+}
+
+// \param[in] speed [-100,100]
+function setFwdBackMotorSpeed(speed) {
+
+  // Round to the fist decimal (this will give us the resolution of 1000 levels of outputs)
+  speed = Number.parseFloat(speed).toFixed(1);
+
+  var msg = 'f' + speed.toString();
+  console.log("ws msg (f/b): " + msg);
+  if(myWebSocket != null && myWebSocket.readyState == WebSocket.OPEN) {
+    myWebSocket.send(msg);
+  }
+}
+
+function setSteering(steering) {
+
+  // Round to the fist decimal (this will give us the resolution of 1000 levels of outputs, i.e. [-100.0,100.0])
+  steering = Number.parseFloat(steering).toFixed(1);
+
+  var msg = 's' + steering.toString();
+  console.log("ws msg (steering): " + msg);
+  if(myWebSocket != null && myWebSocket.readyState == WebSocket.OPEN) {
+    myWebSocket.send(msg);
+  }
+}
