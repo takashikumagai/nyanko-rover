3b57c842b8aebd7ac61e5f481223ea3c4ad9fa73
nyankosoft <sgt.nyanko@gmail.com>
2018-05-20 19:28:47 +0900

    Split get_subnet() function. The new get_ip() function and refactored get_subnet() now use the common parsing code of get_ip_and_num_subnet_bits().

--- nyanko-rover-server/networktool.py
+++ nyanko-rover-server/networktool.py
@@ -5,6 +5,17 @@ import re
 
 
 def get_subnet():
+  ip_and_subnetbits = get_ip_and_num_subnet_bits()
+  if ip_and_subnetbits[1] == '24':
+    octets = ip_and_subnetbits[0].split('.')
+    if len(octets) == 4:
+      return '{}.{}.{}'.format(octets[0],octets[1],octets[2])
+
+def get_ip():
+  ip_and_subnetbits = get_ip_and_num_subnet_bits()
+  return ip_and_subnetbits[0]
+
+def get_ip_and_num_subnet_bits():
   completed_process = subprocess.run(['ip','addr'], stdout=subprocess.PIPE)
   lines = completed_process.stdout.decode('utf-8').split('\n')
   found_network_adapter = False
@@ -27,11 +38,10 @@ def get_subnet():
         ip_and_subnetbits = words[1].split('/')
         #print('ip & subnet bits: {}'.format(ip_and_subnetbits))
         if 1 < len(ip_and_subnetbits):
-          if ip_and_subnetbits[1] == '24':
-            octets = ip_and_subnetbits[0].split('.')
-            if len(octets) == 4:
-              return '{}.{}.{}'.format(octets[0],octets[1],octets[2])
-      return ''
+          return ip_and_subnetbits
+        else:
+          return ['x.x.x.x','0']
+      return ['#.#.#.#','0']
 
   
 
@@ -51,6 +61,7 @@ def get_connected_devices():
   scan_results = scan_with_nmap(get_subnet())
   ip_addresses = []
   for line in scan_results:
+    #print(line)
     if line.startswith('Nmap scan report for'):
       ip = line[len('Nmap scan report for'):]
       ip_addresses.append(ip)
@@ -59,16 +70,29 @@ def get_connected_devices():
   #for ip in ip_addresses:
   #  completed_process = subprocess.run(['ping','-c','1',ip], stdout=subprocess.PIPE)
   
+  #print('running arp')
   # Now the devices in the specified subnet have been pinged,
   # they should show up in the output of arp -a with their MAC addresses
   cp = subprocess.run(['arp','-a'], stdout=subprocess.PIPE)
   arp_results = cp.stdout.decode('utf-8').split('\n')
+  #print('arp_results: ',arp_results)
   results = [x for x in arp_results if '<incomplete>' not in x]
-  #print(results)
+  
+  # results = []
+  # for x in arp_results:
+  #   try:
+  #     if x.index('<incomplete>') >= 0:
+  #       pass
+  #   except:
+  #     results.append(x)
+
+  #print("filtered arp results: " + str(results))
 
   ip_at_mac = []
   for line in results:
+    #print(line)
     m = re.search('\(.+\)\sat\s[0-9a-f:]+',line)
+    #print(m)
     if not m:
       continue
     ip_at_mac.append(m.group(0))
@@ -84,6 +108,7 @@ def get_connected_devices():
 
 
 def get_ip_from_mac(mac_address):
+  #print('MAC: ', mac_address)
   mac2ip = get_connected_devices()
   return mac2ip.get(mac_address.lower(),'')
 
