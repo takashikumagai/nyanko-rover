17b69617d52bdb32749201ad27dbc49bd19e5634
nyankosoft <sgt.nyanko@gmail.com>
2018-07-18 22:43:51 +0900

    Added stub classes to run the application locally for testing purposes (without using a real Raspberry Pi).

---
+++ run_local_test_server.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp -f stub/motor_control-stub.py nyanko-rover-server/motor_control.py
+cp -f stub/VideoStream-stub.py nyanko-rover-server/VideoStream.py
+cd nyanko-rover-server
+./myserver.py
---
+++ stub/VideoStream-stub.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class VideoStream:
+
+  def __init__(self,video_resolution='640x480'):
+
+  def stop_recording(self):
+    pass
+
+  def start_recording(self,http_handler):
+    pass
