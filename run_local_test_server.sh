8e5ce1f0daf1a6e73c27a96e06739433f7412d98
nyankosoft <sgt.nyanko@gmail.com>
2018-07-23 19:53:49 +0900

    Updated the local server script.

--- run_local_test_server.sh
+++ run_local_test_server.sh
@@ -3,5 +3,5 @@
 cp -f stub/motor_control-stub.py nyanko-rover-server/motor_control.py
 cp -f stub/VideoStream-stub.py nyanko-rover-server/VideoStream.py
 cd nyanko-rover-server
-rm nyankoroverserver.log
+rm -f nyankoroverserver.log
 ./myserver.py
