4ba314f0eabc375931c0859bd6585f8f397741cb
nyankosoft <sgt.nyanko@gmail.com>
2018-08-06 07:59:04 +0900

    Added stub of vcgencmd.py

--- run_local_test_server.sh
+++ run_local_test_server.sh
@@ -2,7 +2,8 @@
 
 rm -rf __pycache__
 cp -f stub/motor_control-stub.py nyanko-rover-server/motor_control.py
-cp -f stub/VideoStream-stub.py nyanko-rover-server/VideoStream.py
+cp -f stub/VideoStream-stub.py   nyanko-rover-server/VideoStream.py
+cp -f stub/vcgencmd-stub.py      nyanko-rover-server/vcgencmd.py
 cd nyanko-rover-server
 rm -rf __pycache__
 rm -f nyankoroverserver.log
---
+++ stub/vcgencmd-stub.py
@@ -0,0 +1,14 @@
+def measure_measure_clock():
+    return 50
+
+def measure_temp():
+    return 50
+
+def codec_sources():
+    return []
+
+def codec_enabled(src):
+    return False
+
+def get_camera(src):
+    return 0
