82ba403cdf875cbfdb04c905522f05d42aaef467
nyankosoft <sgt.nyanko@gmail.com>
2018-09-16 12:53:49 +0900

    Updated the local server script.

--- run_local_test_server.sh
+++ run_local_test_server.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 rm -rf __pycache__
-cp -f stub/motor_control-stub.py nyanko-rover-server/motor_control.py
-cp -f stub/RPiCamera-stub.py     nyanko-rover-server/RPiCamera.py
-cp -f stub/vcgencmd-stub.py      nyanko-rover-server/vcgencmd.py
+cp -f stub/motor_control-stub.py   nyanko-rover-server/motor_control.py
+cp -f stub/RPiCamera-stub.py       nyanko-rover-server/RPiCamera.py
+cp -f stub/vcgencmd-stub.py        nyanko-rover-server/vcgencmd.py
+cp -f stub/server_params-stub.json nyanko-rover-server/server_params.json
+
 cd nyanko-rover-server
 rm -rf __pycache__
 rm -f nyankoroverserver.log
