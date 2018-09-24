a42722799f5379c3ef0f3a0b674bb125784eae66
nyankosoft <sgt.nyanko@gmail.com>
2018-09-24 17:23:03 +0900

    Ctor takes a list of static image file names for setting up a fake camera.

--- nyanko-rover-server/TestImageCamera.py
+++ nyanko-rover-server/TestImageCamera.py
@@ -6,8 +6,21 @@ class TestImageCamera:
     with open(pathname,'rb') as f:
       return f.read()
 
-  def __init__(self):
-    self.frames = [self.read_file('sample_images/'+f+'.jpg') for f in ['img1','img2','img3']]
+  def __init__(self,filename_list):
+    #self.frames = [self.read_file('sample_images/'+f+'.jpg') for f in ['img1','img2','img3']]
+    self.frames = [self.read_file('sample_images/'+f) for f in filename_list]
+
+  def start_capture(self):
+    print('(TestImageCamera) starting video capture - nothing to do')
+    pass
+
+  def stop_capture(self):
+    print('(TestImageCamera) stopping video capture - nothing to do')
+    pass
+
+  def close(self):
+    print('(TestImageCamera) closing the device - nothing to do')
+    pass
 
   def get_frame(self):
     time.sleep(0.05)
