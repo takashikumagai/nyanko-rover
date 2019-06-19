dfeb2ac5eeb09c8010e9fe5441cbe18c8d829bec
nyankophile <nyankophile@gmail.com>
2019-06-20 02:19:24 +0900

    Added a 'stop_server' command for the shell.

--- nyanko-rover-server/server_utils/stop_server.sh
+++ nyanko-rover-server/server_utils/stop_server.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+ps -aux | grep -E -m 1 nyanko-rover-server.+flask.+run
+
 p=$(ps -aux | grep -E -m 1 nyanko-rover-server.+flask.+run)
-q={$p}
+
+# Convert the string into an array
+q=($p)
 
 echo "Terminating the process"
 sudo pkill -f ${q[1]}
 
 ps -aux | grep -E -m 1 nyanko-rover-server.+flask.+run
+
