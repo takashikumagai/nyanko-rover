5976c433d2c6d53038bc6abe6d385697fcc1c423
nyankophile <nyankophile@gmail.com>
2020-03-08 11:08:04 +0900

    Added a stub RPiCamera module.

---
+++ stub/RPiCamera-stub.py
@@ -0,0 +1,14 @@
+class RPiCamera:
+
+  def start_capture(self):
+    logging.error('!!!!!!!!!! picamera is only available on Raspberry Pi !!!!!!!!!!')
+
+  def start_capture(self):
+    logging.error('!!!!!!!!!! picamera is only available on Raspberry Pi !!!!!!!!!!')
+
+  def close(self):
+    logging.error('!!!!!!!!!! picamera is only available on Raspberry Pi !!!!!!!!!!')
+
+  def get_frame(self):
+    logging.error('!!!!!!!!!! picamera is only available on Raspberry Pi !!!!!!!!!!')
+    return 0
