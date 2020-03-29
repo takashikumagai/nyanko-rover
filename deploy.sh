af7e334d3523dd553ed9edb2ab0ab3dae5355e0f
nyankophile <nyankophile@gmail.com>
2020-03-29 14:57:10 +0900

    Updated the push script (Tamiya RC car ESC controller). Note that it now also requires the user to specify the ssh remote host as the first argument.
    Rationale: managing ssh remote host and options is not the job of this app. Also, some of the prominent tools make use of .ssh/config, e.g. rsync and sshfs, so using them the same way will keep things in line with other tools.

--- deploy.sh
+++ deploy.sh
@@ -8,25 +8,22 @@
 # 1. Load configuration parameters (server IP address, ssh key path, etc.) from nyanko-rover-config.json
 #
 
+ssh_remote_host=$1
+
+if [ -z "${ssh_remote_host}" ]; then
+    echo "Specify an ssh host defined in ~/.ssh/config as the first argument"
+    exit 1
+fi
+
+# First user_env_var_on_remote_host will get a value like USER=someuser
+user_env_var_on_remote_host=$(ssh ${ssh_remote_host} printenv | grep USER=)
+user=${user_env_var_on_remote_host:5}
+
 if [ ! -f nyanko-rover-config.json ]; then
     cp nyanko-rover-config.json.example nyanko-rover-config.json
     echo "Set the IP address of your RPi, and other properties to nyanko-rover-config.json and run this script again."
     exit 0
 fi
-
-# Read the first line of the text file and store it to a variable
-server_ip_address=$(cat nyanko-rover-config.json | python3 -c "import sys, json; print(json.load(sys.stdin)['server_ip_address'])")
-
-# SSH port
-ssh_port=$(cat nyanko-rover-config.json | python3 -c "import sys, json; print(json.load(sys.stdin)['ssh_port'])")
-
-# Read the pathname of the SSH key file
-ssh_key=$(cat nyanko-rover-config.json | python3 -c "import sys, json; print(json.load(sys.stdin)['ssh_key'])")
-
-# RPi login user
-user=$(cat nyanko-rover-config.json | python3 -c "import sys, json; print(json.load(sys.stdin)['login_user'])")
-
-echo $server_ip_address, $ssh_port, $ssh_key, $user
 
 #
 # 2. If the user specified '--clean' option, delete the current application on the server
@@ -35,7 +32,7 @@ echo $server_ip_address, $ssh_port, $ssh_key, $user
 # Note that we need to double quote the commands so that the bash shell will
 # interpolate the $user before sending the command string to the remote host.
 if [ "$1" = "--clean" ]; then
-  ssh -i $ssh_key -o IdentitiesOnly=yes $user@$server_ip_address -p $ssh_port "rm -rf /home/$user/nyanko-rover-server"
+  ssh ${ssh_remote_host} "rm -rf /home/${user}/nyanko-rover-server"
 fi
 
 #
@@ -45,23 +42,29 @@ fi
 # Make sure no trash will be sent to the server.
 rm -f nyanko-rover-server/nyankoroverserver.log
 
+# Send the package to the server
+rsync -Parvzz nyanko-rover-server ${ssh_remote_host}:/home/${user}/
+
+remote_host_dir=/home/${user}/nyanko-rover-server
+
 # Copy the RPi-specific modules
 # Replace the stub motor control functions (motor_control.py) with real ones (motor_control-rpi.py)
-cp -f rpi/motor_control-rpi-queue.py nyanko-rover-server/motor_control.py
-cp -f rpi/vcgencmd-rpi.py            nyanko-rover-server/vcgencmd.py
-cp -f rpi/RPiCamera-rpi.py           nyanko-rover-server/camera/RPiCamera.py
-cp -f rpi/server_params-rpi.json     nyanko-rover-server/server_params.json
-
-# Send the package to the server
-rsync -Parvz -e "ssh -i $ssh_key -o IdentitiesOnly=yes" nyanko-rover-server $user@$server_ip_address:/home/$user/
+rsync -Parvzz rpi/motor_control-rpi-queue.py    ${ssh_remote_host}:${remote_host_dir}/motor_control.py
+rsync -Parvzz rpi/vcgencmd-rpi.py               ${ssh_remote_host}:${remote_host_dir}/vcgencmd.py
+rsync -Parvzz rpi/RPiCamera-rpi.py              ${ssh_remote_host}:${remote_host_dir}/camera/RPiCamera.py
+rsync -Parvzz rpi/server_params-rpi.json        ${ssh_remote_host}:${remote_host_dir}/server_params.json
+# rsync these files the same way but no name changes with them
+rsync -Parvzz rpi/motor_controller_TEU_105BK.py ${ssh_remote_host}:${remote_host_dir}/
+rsync -Parvzz rpi/motor_controller_L293D.py     ${ssh_remote_host}:${remote_host_dir}/
+rsync -Parvzz rpi/esc_tester.py                 ${ssh_remote_host}:${remote_host_dir}/
 
 # Uncomment the RPi.GPIO dependency in Pipfile, i.e. replace '#RPi.GIPO' to 'RPi.GPIO'
 # Note that backslashes are necessary to escape double quotes because sed command is double quoted.
 # When you execute sed from terminal, they do not have to be escaped.
-ssh -i $ssh_key -o IdentitiesOnly=yes $user@$server_ip_address -p $ssh_port "sed -i 's/^#\"RPi\.GPIO\"/\"RPi\.GPIO\"/' /home/$user/nyanko-rover-server/Pipfile"
+ssh ${ssh_remote_host} "sed -i 's/^#\"RPi\.GPIO\"/\"RPi\.GPIO\"/' ${remote_host_dir}/Pipfile"
 
 # Similarly, uncomment the #picamera to picamera
-ssh -i $ssh_key -o IdentitiesOnly=yes $user@$server_ip_address -p $ssh_port "sed -i 's/^#picamera/picamera/' /home/$user/nyanko-rover-server/Pipfile"
+ssh ${ssh_remote_host} "sed -i 's/^#picamera/picamera/' ${remote_host_dir}/Pipfile"
 
 # Delete the old log file if there is one so that new log entries will not be appended to it.
-ssh -i $ssh_key -o IdentitiesOnly=yes $user@$server_ip_address -p $ssh_port "rm -f /home/$user/nyanko-rover-server/nyankoroverserver.log"
+ssh ${ssh_remote_host} "rm -f ${remote_host_dir}/nyankoroverserver.log"
